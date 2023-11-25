5b57ce1b9b209d5683fec5c60bdeb62de50dfa4e
alexpark-101 <alex.park@101.inc>
2023-11-24 17:35:26 -0800

    fix main

--- app/Main.hs
+++ app/Main.hs
@@ -9,10 +9,10 @@ import           UI                        (ui)
 argparse :: Parser Arguments
 argparse =
   Arguments
-    <$> option parseMode (long "mode" <> short 'm' <> value Random <> help "possible arguments: quote, random, timed")
-    <*> option auto (long "time" <> short 't' <> value 30 <> showDefault <> help "How long the test should run, only affects timed mode")
-    <*> option auto (long "line_length" <> short 'l' <> value 12 <> showDefault <> help "Number of words to display per line")
-    <*> option auto (long "num_words" <> short 'n' <> value 36 <> showDefault <> help "Number of Words to randomly select")
+    <$> option parseMode (long "mode" <> short 'm' <> value Random)
+    <*> option auto (long "time" <> short 't' <> value 30 <> showDefault)
+    <*> option auto (long "line_length" <> short 'l' <> value 12 <> showDefault)
+    <*> option auto (long "num_words" <> short 'n' <> value 36 <> showDefault)
 
 parseMode :: ReadM Mode
 parseMode = do
@@ -29,7 +29,7 @@ main = do
   conf <- readConfig
   runTest conf =<< execParser opts
     where
-      opts = info (argparse <**> helper) (fullDesc <> progDesc "A cli-based typing test written in haskell" <> header "typeracer")
+      opts = info (argparse <**> helper) (fullDesc <> progDesc "A cli-based typing game written in haskell" <> header "typeracer")
 
 
 runTest :: Conf -> Arguments ->IO ()
